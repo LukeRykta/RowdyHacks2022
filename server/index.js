802debd940634947c82998d630352357c62cf852
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-03-26 13:13:38 -0500

    update to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,33 @@
-const express = require('express');
+//Global variables that allow index.js to use libraries for server.
+const app = require('express');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const uuid = require('node-uuid');
 const Room = require('./room.js');
+const _ = require('underscore')._;
 
+/*
+  Global variable/arrays that will store important user data
+  that will be used through many socket events.
+*/
+let players = [];
+let person = [];
+let rooms = [];
+
+//variables that might be used.
+let roomLimit = 0;
+
+server.listen(3000, function(){
+    console.log("Server is now running...");
+});
+
+io.on('connection', function(socket){
+
+
+
+
+
+
+
+
+});
