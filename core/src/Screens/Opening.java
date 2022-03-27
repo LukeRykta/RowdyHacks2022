a24e165aaa05ea3306bd2d47a86f98ecaa0f1ce6
Luke <80142097+LukeRykta@users.noreply.github.com>
2022-03-26 22:28:46 -0500

    Animations refactoring

--- core/src/Screens/Opening.java
+++ core/src/Screens/Opening.java
@@ -8,7 +8,9 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -17,9 +19,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.reflect.ReflectionException;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import mygdx.game.RhythmGame;
 
@@ -28,6 +30,10 @@ import static com.badlogic.gdx.utils.Align.center;
 public class Opening extends AbstractScreen implements InputProcessor {
     private OrthographicCamera cam;
     private Stage stage;
+
+    private Animation runAnimation; // animation key frames
+    private Texture blueDinoSheet; // loaded image sheet (png)
+
     private TextField name;
     private SpriteBatch batch;
     private Label presentInfo;
@@ -35,13 +41,25 @@ public class Opening extends AbstractScreen implements InputProcessor {
     private Skin skin;
     private Music music;
 
+    float stateTime;
+
+    private Texture blueDino;
+
     protected Sound forwardSound;
 
     public Opening(RhythmGame context) {
         super(context);
+        createCamera();
         initMusic();
+        initTextures();
         initSkin();
         initStage();
+    }
+
+    public void createCamera(){
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, RhythmGame.V_WIDTH, RhythmGame.V_HEIGHT);
+        //gamePort = new StretchViewport(RhythmGame.V_WIDTH , RhythmGame.V_HEIGHT , gamecam);
     }
 
     public void initMusic(){
@@ -49,6 +67,21 @@ public class Opening extends AbstractScreen implements InputProcessor {
         music = manager.get("music/songs/menu.mp3");
         forwardSound = manager.get("music/sounds/forward.wav");
         music.setVolume(.5f);
+    }
+
+    public void initTextures(){
+        blueDinoSheet = new Texture("uiGFX/texturepacks/blueDino.png"); // set file path for png
+        TextureRegion[][] tmp = TextureRegion.split(blueDinoSheet, blueDinoSheet.getWidth() / 24, blueDinoSheet.getHeight()); // declare region size
+
+        TextureRegion[] runFrames = new TextureRegion[11];
+        int index = 0;
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 11; j++) {
+                runFrames[index++] = tmp[i][j];
+            }
+        }
+
+        runAnimation = new Animation<TextureRegion>(0.1f, runFrames);
     }
 
     public void initSkin(){
@@ -59,6 +92,7 @@ public class Opening extends AbstractScreen implements InputProcessor {
 
     public void initStage(){
         stage = new Stage (new ScreenViewport(cam));
+        batch = new SpriteBatch();
 
         final Table backgroundTable = new Table(skin); // Table for background
         backgroundTable.setBackground(new TextureRegionDrawable(new TextureRegion(new Texture("uiGFX/backgrounds/bg.png"))));
@@ -118,12 +152,18 @@ public class Opening extends AbstractScreen implements InputProcessor {
         //Gdx.gl.glClearColor(0xC7,  0xFB, 0x50, 1/4); //green
         //Gdx.gl.glClearColor(0x38,  0x95, 0xF7, 1/4); //blue
         //Gdx.gl.glClearColor(0xF5,  0xB0, 0x3E, 1/4); //orange
+        stateTime += delta;
+        TextureRegion currentFrame = (TextureRegion) runAnimation.getKeyFrame(stateTime, true); // get the key frame based on the state time
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-
         //stage.act(delta);
         stage.draw();
+
+        batch.setProjectionMatrix(cam.combined);
+
+        batch.begin();
+        batch.draw(currentFrame, stage.getWidth() / 4, stage.getHeight()/2 - 128, 256, 256); // draw x and y position and scale size
+        batch.end();
 
         try {
             update(delta);
